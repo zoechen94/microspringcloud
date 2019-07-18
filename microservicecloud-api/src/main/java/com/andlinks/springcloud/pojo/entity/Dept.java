57f071bebd9ed962da9706e3d795fb6d9f826ac8
chenyalan <chenyalan@andlinks.com>
2019-07-18 11:38:43 +0800

    添加

--- microservicecloud-api/src/main/java/com/andlinks/springcloud/pojo/entity/Dept.java
+++ microservicecloud-api/src/main/java/com/andlinks/springcloud/pojo/entity/Dept.java
@@ -1,10 +1,5 @@
 package com.andlinks.springcloud.pojo.entity;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
-
-@Data
-@Accessors(chain = true)
 public class Dept {
     private Long deptno;
 
--- springcloud-study-provider-dept-8001/src/main/java/com/andlinks/dept8001/controller/TestController.java
+++ springcloud-study-provider-dept-8001/src/main/java/com/andlinks/dept8001/controller/TestController.java
@@ -1,17 +1,19 @@
 package com.andlinks.dept8001.controller;
 
 import com.andlinks.dept8001.service.Test;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(value = "con")
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @Autowired
     private Test test;
+@ApiOperation("测试")
     @GetMapping("/add")
     public void setTest(){
         test.insert();
